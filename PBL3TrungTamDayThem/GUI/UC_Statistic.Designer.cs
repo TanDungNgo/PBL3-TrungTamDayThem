eca23846be143c58050362a78d63a6c56cf6d188
Tan Dung <tandung12072002@gmail.com>
2022-06-06 00:11:17 +0700

    fix phóng to

--- PBL3TrungTamDayThem/GUI/UC_Statistic.Designer.cs
+++ PBL3TrungTamDayThem/GUI/UC_Statistic.Designer.cs
@@ -34,12 +34,12 @@
             this.pnlHeader = new System.Windows.Forms.Panel();
             this.lblHeader = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblYear = new System.Windows.Forms.Label();
             this.btnTKMH = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.btnTKDT = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.lblYear = new System.Windows.Forms.Label();
             this.pnlHeader.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -80,10 +80,21 @@
             this.panel1.Size = new System.Drawing.Size(907, 128);
             this.panel1.TabIndex = 28;
             // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYear.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblYear.Location = new System.Drawing.Point(23, 29);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(60, 25);
+            this.lblYear.TabIndex = 44;
+            this.lblYear.Text = "Năm:";
+            // 
             // btnTKMH
             // 
             this.btnTKMH.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTKMH.Location = new System.Drawing.Point(511, 28);
+            this.btnTKMH.Location = new System.Drawing.Point(397, 29);
             this.btnTKMH.Name = "btnTKMH";
             this.btnTKMH.Size = new System.Drawing.Size(175, 73);
             this.btnTKMH.TabIndex = 2;
@@ -96,7 +107,7 @@
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(128, 66);
+            this.comboBox1.Location = new System.Drawing.Point(28, 67);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(159, 35);
             this.comboBox1.TabIndex = 1;
@@ -104,7 +115,7 @@
             // btnTKDT
             // 
             this.btnTKDT.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTKDT.Location = new System.Drawing.Point(320, 28);
+            this.btnTKDT.Location = new System.Drawing.Point(206, 29);
             this.btnTKDT.Name = "btnTKDT";
             this.btnTKDT.Size = new System.Drawing.Size(175, 73);
             this.btnTKDT.TabIndex = 0;
@@ -137,17 +148,6 @@
             this.chart1.Size = new System.Drawing.Size(907, 433);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
-            // 
-            // lblYear
-            // 
-            this.lblYear.AutoSize = true;
-            this.lblYear.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblYear.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblYear.Location = new System.Drawing.Point(123, 28);
-            this.lblYear.Name = "lblYear";
-            this.lblYear.Size = new System.Drawing.Size(60, 25);
-            this.lblYear.TabIndex = 44;
-            this.lblYear.Text = "Năm:";
             // 
             // UC_Statistic
             // 
